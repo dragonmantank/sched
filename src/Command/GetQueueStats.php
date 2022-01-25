0a528adb9121426fe0e8e5db661b841fb0f8bd1c
Chris Tankersley <chris@ctankersley.com>
2022-01-25 12:12:57 -0500

    Added ability to see condensed queue stats on all queues

--- src/Command/GetQueueStats.php
+++ src/Command/GetQueueStats.php
@@ -7,6 +7,7 @@ namespace Dragonmantank\Sched\Command;
 use Pheanstalk\Exception\ServerException;
 use Pheanstalk\Pheanstalk;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,7 +17,19 @@ class GetQueueStats extends Command
 {
     protected static $defaultName = 'queue:stats';
 
+    /**
+     * @param array{
+     *      'pheanstalk': array<string, mixed>,
+     *      'cron': array<
+     *          int,
+     *          array{'name': string, 'expression': string, 'worker': string|callable}
+     *      >,
+     *      'queues': array<string, array{'worker': string|callable}>,
+     *      'config': array{'path': string}
+     * } $config
+     */
     public function __construct(
+        protected array $config,
         protected Pheanstalk $pheanstalk
     ) {
         parent::__construct();
@@ -26,30 +39,50 @@ class GetQueueStats extends Command
     {
         $this
             ->setHelp('Returns information about the queue')
-            ->addArgument('queueName', InputArgument::REQUIRED, 'Queue to check');
+            ->addArgument('queueName', InputArgument::OPTIONAL, 'Queue to check');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var string */
         $queueName = $input->getArgument('queueName');
-
-        try {
-            $stats = $this->pheanstalk->statsTube($queueName);
-        } catch (ServerException $e) {
-            $output->writeln('Unable to get stats, tube does not exist');
-            return Command::FAILURE;
-        }
-
         $rows = [];
-        foreach ($stats as $key => $value) {
-            $rows[] = [$key, $value];
+        $headers = [];
+
+        if ($queueName) {
+            $headers = ['Variable', 'Value'];
+            try {
+                $stats = $this->pheanstalk->statsTube($queueName);
+            } catch (ServerException $e) {
+                $output->writeln('Unable to get stats, tube does not exist');
+                return Command::FAILURE;
+            }
+
+            $rows = [];
+            foreach ($stats as $key => $value) {
+                $rows[] = [$key, $value];
+            }
+        } else {
+            $headers = ['Queue', 'Stats'];
+            foreach ($this->config['queues'] as $queueName => $_) {
+                try {
+                    $stats = $this->pheanstalk->statsTube($queueName);
+                } catch (ServerException $e) {
+                    $rows[] = [$queueName, 'Tube is empty'];
+                    continue;
+                }
+                $rows[] = [$queueName, "current-jobs-ready: " . $stats['current-jobs-ready']];
+                $rows[] = ['', "current-jobs-reserved: " . $stats['current-jobs-reserved']];
+                $rows[] = ['', "current-watching: " . $stats['current-watching']];
+            }
         }
+
         $table = new Table($output);
         $table
-            ->setHeaders(['Variable', 'Value'])
+            ->setHeaders($headers)
             ->setRows($rows);
         $table->render();
+
         return Command::SUCCESS;
     }
 }
