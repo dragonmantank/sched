694f4e034d9627489f50d0c26b924509cbb3d4c5
Eli White <eli@eliw.com>
2023-03-21 09:51:50 -0400

    Allows direct Monolog/Logger di, and a better worker-error message

--- config/di.php
+++ config/di.php
@@ -9,9 +9,11 @@ use Dragonmantank\Sched\Factory\QueueServiceFactory;
 use Dragonmantank\Sched\Queue\QueueService;
 use Pheanstalk\Pheanstalk;
 use Psr\Log\LoggerInterface;
+use Monolog\Logger;
 
 return [
     'sched-config' => DI\factory(ConfigFactory::class),
+    Logger::class => DI\factory(LoggerInterfaceFactory::class),
     LoggerInterface::class => DI\factory(LoggerInterfaceFactory::class),
     Pheanstalk::class => DI\factory(PheanstalkFactory::class),
     QueueService::class => DI\factory(QueueServiceFactory::class),
